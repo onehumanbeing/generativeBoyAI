5c2b46a09c44bb10c558ae8c51dac7870b0d4055
FrankLi123 <zoli@ucsd.edu>
2024-03-01 05:39:34 -0700

    update backend code

--- src/backend/api/index.py
+++ src/backend/api/index.py
@@ -2,14 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import json
-from vercel_kv import KV, Opts
+# from vercel_kv import KV, Opts
 import os
 
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 API_KEY = os.getenv("OPENAI", 'your_password')
 account = os.getenv("GASTANK_ACCOUNT", 'your_password')
 private_key = os.getenv("GASTANK_PK", 'your_password')
-CACHE = KV()
+# CACHE = KV()
 
 headers = {
     'Authorization': f'Bearer {API_KEY}',
@@ -24,8 +24,9 @@ def ask_gpt3(messages):
     }
     response = requests.post(API_ENDPOINT, headers=headers, json=data)
     response_json = response.json()
-
+    print("response_json", response_json)
     if response.status_code == 200:
+        print("ask_gpt return value:",response_json['choices'][0]['message']['content'].strip())
         return response_json['choices'][0]['message']['content'].strip()
     else:
         print(f"Error {response.status_code}: {response_json['error']['message']}")
@@ -40,12 +41,12 @@ def permit():
 
 @app.route('/agent', methods=['POST'])
 def agent():
-    data = request.json()
+    data = request.json
     messages = data['messages']
     bountry = int(data['bountry'])
-    prompt = CACHE.get(key="prompt_bountry_%d" % bountry)
-    if prompt is not None:
-        messages = prompt + messages
+    # prompt = CACHE.get(key="prompt_bountry_%d" % bountry)
+    # if prompt is not None:
+    # messages = prompt + messages
     response = ask_gpt3(messages)
     messages.append({
         "role": "assistant",
@@ -55,10 +56,10 @@ def agent():
 
 @app.route('/prompt', methods=['POST'])
 def prompt():
-    data = request.json()
+    data = request.json
     bountry = int(data['bountry'])
     prompt = data['prompt']
-    CACHE.put(key="prompt_bountry_%d" % bountry, value=prompt)
+    # CACHE.put(key="prompt_bountry_%d" % bountry, value=prompt)
     return jsonify({'response': 'success'}), 200
 
 @app.route('/')
