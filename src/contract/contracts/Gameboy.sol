422445de6419c2f1be7801b2e17359839f3c20ea
Lanyin Z <106770708+lanyinzly@users.noreply.github.com>
2024-03-01 17:03:17 -0700

    Update Gameboy.sol

--- src/contract/contracts/Gameboy.sol
+++ src/contract/contracts/Gameboy.sol
@@ -21,7 +21,7 @@ interface IERC20Permit{
     function transfer(address to, uint256 amount) external returns (bool);
 }
 
-contract GameBoy is ERC721, Ownable {
+contract GameBoy is ERC721 {
     using Strings for uint256;
     address payable public bot;
     mapping(uint256 => Bounty) public bounties;
@@ -29,7 +29,7 @@ contract GameBoy is ERC721, Ownable {
     uint256[] public activeBounties;
 
     struct Bounty {
-        uint256 amount;
+        uint256 bountyAmount;
         uint256 ticketAmount;
         IERC20Permit token;
         address payable creator;
@@ -45,15 +45,16 @@ contract GameBoy is ERC721, Ownable {
         bot = _bot;
     }
 
-    function setBot(address payable _bot) onlyOwner() external {
+    function setBot(address payable _bot) external {
         bot = _bot;
     }
 
     function permitWithCreateBounty(
         string memory answer, 
         IERC20Permit token, 
-        uint256 amount, 
+        uint256 permitAmount, 
         uint256 ticketAmount,
+        uint256 bountyAmount,
         string memory ipfsHash,
         uint256 deadline,
         address payable from,
@@ -62,15 +63,15 @@ contract GameBoy is ERC721, Ownable {
         bytes32 s    
     ) external {
         require(msg.sender == bot, "auth");
-        require(amount > 0, "Bounty amount must be greater than 0");
-        token.permit(from, address(this), amount, deadline, v, r, s);
+        require(bountyAmount > 0, "Bounty amount must be greater than 0");
+        token.permit(from, address(this), permitAmount, deadline, v, r, s);
         uint256 salt = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender)));
         bytes32 hashedAnswer = keccak256(abi.encodePacked(answer, salt.toString()));
-        bounties[nextBountyId] = Bounty(amount, ticketAmount, token, payable(msg.sender), hashedAnswer, ipfsHash, false, salt);
+        bounties[nextBountyId] = Bounty(bountyAmount, ticketAmount, token, payable(msg.sender), hashedAnswer, ipfsHash, false, salt);
         activeBounties.push(nextBountyId);
         nextBountyId++;
         // Transfer the bounty amount to the GameBoy contract to activate the bounty
-        token.transferFrom(msg.sender, address(this), amount);
+        token.transferFrom(msg.sender, address(this), bountyAmount);
     }
 
     function verify(uint256 bountyId, string memory answer, address from) external {
@@ -80,7 +81,7 @@ contract GameBoy is ERC721, Ownable {
         if (keccak256(abi.encodePacked(answer, bounty.salt.toString())) == bounty.hashedAnswer) {
             bounty.claimed = true;
             _burn(bountyId);
-            bounty.token.transfer(from, bounty.amount);
+            bounty.token.transfer(from, bounty.bountyAmount);
             // Remove the bounty from the active bounties list
             for (uint i = 0; i < activeBounties.length; i++) {
                 if (activeBounties[i] == bountyId) {
@@ -98,7 +99,7 @@ contract GameBoy is ERC721, Ownable {
             bounty.token.transferFrom(from, bounty.creator, creatorReward);
             bounty.token.transferFrom(from, bot, botReward);
             bounty.token.transferFrom(from, address(this), bountyIncrease);
-            bounty.amount += bountyIncrease;
+            bounty.bountyAmount += bountyIncrease;
         }
     }
 
@@ -106,27 +107,27 @@ contract GameBoy is ERC721, Ownable {
         Bounty[] memory activeBountyDetails = new Bounty[](activeBounties.length);
         for (uint i = 0; i < activeBounties.length; i++) {
             Bounty storage bounty = bounties[activeBounties[i]];
-            activeBountyDetails[i] = Bounty(bounty.amount, bounty.ticketAmount, bounty.token, bounty.creator, bounty.hashedAnswer, bounty.ipfsHash, bounty.claimed, 0);
+            activeBountyDetails[i] = Bounty(bounty.bountyAmount, bounty.ticketAmount, bounty.token, bounty.creator, bounty.hashedAnswer, bounty.ipfsHash, bounty.claimed, 0);
         }
         return activeBountyDetails;
     }
 
-    function sendETH(address payable to, uint amount) internal {
-        (bool sent,) = to.call{value: amount}("");
+    function sendETH(address payable to, uint bountyAmount) internal {
+        (bool sent,) = to.call{value: bountyAmount}("");
         require(sent, "Failed to send Ether");
     }
 
     function permit(
         IERC20Permit token,
         address payable from,
-        uint256 amount,
+        uint256 permitAmount,
         uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external {
         require(msg.sender == bot, "auth");
-        token.permit(from, address(this), amount, deadline, v, r, s);
+        token.permit(from, address(this), permitAmount, deadline, v, r, s);
     }
 
     fallback() payable external {} // fallback is cheaper than receive by 26 gas
